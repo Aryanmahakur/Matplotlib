36172a23c0e8fb0c42a13c82bbbff0e9717d2acc
Aryanmahakur <mahakuraryan@gmail.com>
2025-03-06 19:59:53 +0530

    uf

--- matplotlib1.py
+++ matplotlib1.py
@@ -1,6 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.random.rand(50)*100
-y=np.random.rand(50)*100
-plt.scatter(x,y)
+#scatter plot
+# x=np.random.rand(50)*100
+# y=np.random.rand(50)*100
+# plt.scatter(x,y,c="yellow",marker="o",s=105,alpha=0.5)
+# plt.show()
+
+#line plot
+
+
+# years = [2005 + x for x in range(10)]
+# weights = [10, 20, 30, 40, 15, 60, 70, 80, 90, 100]
+
+# plt.plot(years, weights,c="yellow",marker="o",ms=10,ls="dashed",lw=2)
+# plt.xlabel("Years")
+# plt.ylabel("Weights")
+# plt.title("Yearly Weight Progression")
+# plt.show()
+
+#bar plot
+x=["c++","c","java","python"]
+y=[10,20,30,40]
+plt.bar(x,y)
 plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+x=np.random.rand(50)*100
+# y=np.random.rand(50)*100
+# plt.scatter(x,y,c="yellow",marker="o",s=105,alpha=0.5)
+# plt.show()
