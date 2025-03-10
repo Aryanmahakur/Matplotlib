53244a8d757db208c03793207e69b7e933526eea
Aryanmahakur <mahakuraryan@gmail.com>
2025-03-10 22:41:58 +0530

    uf

--- matpractice.py
+++ matpractice.py
@@ -5,9 +5,9 @@ import random
 
 #scatter plot
 
-x=[10,20,30]
-y=[10,50,60]
-plt.scatter(x,y,c="yellow",marker="o",s=105,alpha=0.5)
+years = [2005, 2006, 2007, 2008, 2009]
+weight = [40, 50, 60, 70, 80]
+plt.scatter(years,weight,c="yellow",marker="o",s=105,alpha=0.5)
 #plt.show()
 
 #line plot
@@ -24,8 +24,22 @@ plt.ylabel("Weight (kg)")
 plt.title("Weight Progress Over the Years")
 
 plt.xticks(years)  # Ensure all years are shown on x-axis
-plt.yticks(range(min(weight), max(weight) + 10, 10))  # Adjust y-axis ticks for better readability
+plt.yticks(range(min(weight), max(weight) + 10, 5))  # Adjust y-axis ticks for better readability
 
 plt.grid(True, linestyle="--", alpha=0.6)  # Add grid lines for better visualization
 plt
+plt.show()
+
+#bar plot
+years = [2005, 2006, 2007, 2008, 2009]
+weight = [40, 50, 60, 70, 80]
+
+plt.barh(years,weight,align="center")
+plt.show()
+
+#histogram
+
+years = [2005, 2006, 2007, 2008, 2009]
+weight = [40, 50, 60, 70, 80]
+plt.hist(years,weight,bins=5)
 plt.show()
